cb3dbfcf6a985eaff158ee464e473e675afc8fde
scott <19240759+skaeneman@users.noreply.github.com>
2020-02-07 17:33:38 -0500

    output yelp api data

--- src/bu/met/cs622/YelpInfo.java
+++ src/bu/met/cs622/YelpInfo.java
@@ -1,15 +1,40 @@
 package bu.met.cs622;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class YelpInfo {
     List<YelpInfo> businesses;
     String id;
     String name;
+    String url;
+    Double distance;
+    Object location;
+    List<YelpInfo> categories;
+
+    @SerializedName("is_closed")
+    Boolean isClosed;
+
+    Double rating;
+
+//    String address1;
+
+//    @SerializedName("displayAddress")
+//    String display_address;
 
     public List<YelpInfo> getBusinesses() {
         return businesses;
     }
+
+    public List<YelpInfo> getCategories() {
+        return categories;
+    }
+
+//    public Object getLocation(String address1) {
+//        return this.address1 = address1;
+//    }
+
 
 }
 
